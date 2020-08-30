f11df7a655a82686c0f1576f9912d9a13c7d26c0
Julio Poveda <jjpoveda92@gmail.com>
2020-08-30 11:00:10 -0500

    working on my portfolio

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { IndexHomeComponent } from './components/home/index-home/index-home.component';
+import { TreeExperiencesComponent } from './components/home/tree-experiences/tree-experiences.component';
+import { AboutMeHomeComponent } from './components/home/about-me-home/about-me-home.component';
+import { ServicesHomeComponent } from './components/home/services-home/services-home.component';
+import { ProjectsHomeComponent } from './components/home/projects-home/projects-home.component';
+import { MainFooterComponent } from './components/sections/main-footer/main-footer.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    IndexHomeComponent,
+    TreeExperiencesComponent,
+    AboutMeHomeComponent,
+    ServicesHomeComponent,
+    ProjectsHomeComponent,
+    MainFooterComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/home/tree-experiences/tree-experiences.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-tree-experiences',
+  templateUrl: './tree-experiences.component.html',
+  styleUrls: ['./tree-experiences.component.scss']
+})
+export class TreeExperiencesComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
